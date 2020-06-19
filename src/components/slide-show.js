93ec9c46b19ea1a30598baad27f315469a0bdd76
Djordje Vukovic <djvukovic@outlook.com>
2020-06-19 13:21:18 +0200

    typography

--- src/components/slide-show.js
+++ src/components/slide-show.js
@@ -26,7 +26,7 @@ const SlideShow = props => {
         `)}
         initDelay={2} // delay before the first transition (if left at 0, the first image will be skipped initially)
         transition={4} // transition duration between images
-        duration={8} // how long an image is shown
+        duration={8000} // how long an image is shown
         // specify images to include (and their order) according to `relativePath`
         images={["hero1.jpg", "hero2.jpg", "hero3.jpg"]}
       >
---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from "typography"
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: ["Avenir Next"],
+  bodyFontFamily: ["Roboto"],
+})
+export default typography
