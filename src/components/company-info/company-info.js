ae2e2f89c7ef60d2abc0d5da856397ffb352846a
Djordje Vukovic <djvukovic@outlook.com>
2020-07-03 12:09:05 +0200

    GMaps

--- src/components/company-info/company-info.js
+++ src/components/company-info/company-info.js
@@ -2,16 +2,22 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import { useIntl } from "react-intl"
+import GoogleMapReact from "google-map-react"
 import "./company-info.css"
 
 const CompanyInfo = () => {
   const intl = useIntl()
 
+  const center = {
+    lat: 44.80361,
+    lng: 20.494803,
+  }
+
   const data = useStaticQuery(graphql`
     query {
       background: file(relativePath: { eq: "blue-background.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 2000) {
+          fluid(maxHeight: 700) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -33,15 +39,26 @@ const CompanyInfo = () => {
 
         <p>Svetog Nikole 132/2 , Belgrade, Serbia</p>
 
-        <p>
+        <div>
           <div>Telephone: +381 64 9595 165</div>
           <div>General Inquiries: office@arch-bim.com</div>
-        </p>
+        </div>
 
         <p>
           To reach us at our offices, please check out the map below with the
           address and directions.
         </p>
+      </div>
+
+      <div className="company-info--map">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyCcywKcxXeMZiMwLDcLgyEnNglcLOyB_qw" }}
+          defaultCenter={center}
+          defaultZoom={13}
+        >
+          {/* <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" /> */}
+          <div>nesto</div>
+        </GoogleMapReact>
       </div>
     </div>
   )
