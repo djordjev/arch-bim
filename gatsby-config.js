3c05ac5f9e588fe6867d0a82f7bcc16ebad0af42
Djordje Vukovic <djvukovic@outlook.com>
2020-07-04 11:08:49 +0200

    Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,13 +35,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Arch-BIM Architecture Outsourcing`,
+        short_name: `Arch-BIM`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#29293A`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
