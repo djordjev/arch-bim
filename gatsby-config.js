956d1e5ccaac24ffd57f52be332cd6198c5fb5f7
Djordje Vukovic <djvukovic@outlook.com>
2021-01-09 20:43:00 +0100

    update gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,23 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "UA-171623745-1",
-        head: true,
-        anonymize: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-03T8WPZ5D2", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
       },
     },
     {
